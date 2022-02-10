47fa489c72d84f6ec54c3d77a5331825efb72338
kamishund <kamishund@gmail.com>
2022-02-10 12:39:14 +0900

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/reset.css'
 import '../styles/globals.scss'
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+       <link rel="stylesheet" href="https://use.typekit.net/daq0bze.css" />
+    </Head>
+    <Component {...pageProps} />
+   </>
+  )
 }
 
 export default MyApp
