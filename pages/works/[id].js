1276b9e6e9ee282ebd0a7b054ff32deba68840fd
kamishund <kamishund@gmail.com>
2022-02-12 14:38:32 +0900

    first commit

--- pages/works/[id].js
+++ pages/works/[id].js
@@ -1,12 +1,19 @@
 import Layout from '../../components/Layout'
 import { getAllWorksIds,getAllWorks, getWorksData } from '../../lib/works';
+import { useRouter } from "next/router"
 
 
 export default function WorksDetail({posts}) {
   // console.log(posts)
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+  
   return (
    <Layout>
-    <p>{posts.id}</p>
+    <p>{posts?.id}</p>
    </Layout>
   )
 }
